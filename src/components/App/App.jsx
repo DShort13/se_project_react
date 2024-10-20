85b09af7a69ed27306debee525d231345f3b5d8d
DShort13 <dion.short@outlook.com>
2024-10-20 22:55:29 +0100

    refactor: set up modal form as children prop in App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -15,7 +15,50 @@ function App() {
         <Main weatherData={weatherData} />
       </div>
       <div>
-        <ModalWithForm />
+        <ModalWithForm title="New garment" buttonText="Add garment">
+          <label htmlFor="name" className="modal__label">
+            Name{" "}
+            <input
+              id="name"
+              type="text"
+              className="modal__input"
+              placeholder="Name"
+            />
+          </label>
+          <label htmlFor="imageUrl" className="modal__label">
+            Image{" "}
+            <input
+              id="imageUrl"
+              type="text"
+              className="modal__input"
+              placeholder="Image URL"
+            />
+          </label>
+          <fieldset className="modal__radio-buttons">
+            <legend className="modal__legend">Select the weather type:</legend>
+            <label
+              htmlFor="hot"
+              className="modal__label modal__label_type_radio"
+            >
+              <input id="hot" type="radio" className="modal__radio" />
+              Hot
+            </label>
+            <label
+              htmlFor="warm"
+              className="modal__label modal__label_type_radio"
+            >
+              <input id="warm" type="radio" className="modal__radio" />
+              Warm
+            </label>
+            <label
+              htmlFor="cold"
+              className="modal__label modal__label_type_radio"
+            >
+              <input id="cold" type="radio" className="modal__radio" />
+              Cold
+            </label>
+          </fieldset>
+        </ModalWithForm>
       </div>
     </div>
   );
