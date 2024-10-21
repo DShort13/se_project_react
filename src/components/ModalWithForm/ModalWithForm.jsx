f97ae0e4a8fcfef3518621ffcc9f7b427dce3bb5
DShort13 <dion.short@outlook.com>
2024-10-21 16:12:58 +0100

    make form modal open and close via buttons

--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -1,11 +1,21 @@
 import "./ModalWithForm.css";
 
-function ModalWithForm({ children, title, buttonText }) {
+function ModalWithForm({
+  children,
+  title,
+  buttonText,
+  activeModal,
+  handleCloseClick,
+}) {
   return (
-    <div className="modal">
+    <div className={`modal ${activeModal === "add-garment" && "modal_opened"}`}>
       <div className="modal__content">
         <h2 className="modal__title">{title}</h2>
-        <button className="modal__close"></button>
+        <button
+          onClick={handleCloseClick}
+          type="button"
+          className="modal__close"
+        />
         <form className="modal__form">
           {children}
           <button className="modal__submit">{buttonText}</button>
