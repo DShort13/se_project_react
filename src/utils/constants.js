38f9aabe4f3486d0d4b8423a9406a19dd68e75b8
DShort13 <dion.short@outlook.com>
2024-10-22 20:48:15 +0100

    incorporate OpenWeather API into project

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -36,3 +36,10 @@ export const defaultClothingItems = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Coat.png?etag=298717ed89d5e40b1954a1831ae0bdd4",
   },
 ];
+
+export const coordinates = {
+  latitude: 51.507351,
+  longitude: -0.127758,
+};
+
+export const APIkey = "6fb304c4e6b17ceae0cec788bc8f6a3e";
