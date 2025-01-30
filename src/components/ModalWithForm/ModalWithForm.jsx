1c1e63672d81e8f9bc1bf4ccd33c54aaf20f7cdc
DShort13 <dion.short@outlook.com>
2025-01-30 15:26:33 +0000

    Remove comments

--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -1,13 +1,6 @@
 import "./ModalWithForm.css";
 
-function ModalWithForm({
-  children,
-  title,
-  // buttonText,
-  onClose,
-  isOpen,
-  onSubmit,
-}) {
+function ModalWithForm({ children, title, onClose, isOpen, onSubmit }) {
   return (
     <div className={`modal ${isOpen && "modal_opened"}`}>
       <div className="modal__content">
@@ -15,10 +8,6 @@ function ModalWithForm({
         <button onClick={onClose} type="button" className="modal__close" />
         <form onSubmit={onSubmit} className="modal__form">
           {children}
-          {/* <button type="submit" className="modal__submit">
-            {buttonText}
-            {children}
-          </button> */}
         </form>
       </div>
     </div>
